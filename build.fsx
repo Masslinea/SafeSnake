361002c95415304f6828254215ecb4d26f9bfedb
mtnrbq <rmawston@live.com.au>
2020-07-05 12:52:58 +1000

    Rm00c (#14)
    * auto deploy part 1
    
    * apply tracing for non device auth
    
    * build fixes
    
    * fix up for autodeploy
    
    Co-authored-by: mtnrbq <mtnrbq@gmail.com>

--- build.fsx
+++ build.fsx
@@ -134,18 +134,57 @@ Target.create "ArmTemplate" (fun _ ->
     let armTemplate = @"arm-template.json"
     let resourceGroupName = "safe-" + environment
 
-    let authCtx =
+    // let authCtx =
+    //     // You can safely replace these with your own subscription and client IDs hard-coded into this script.
+    //     let subscriptionId = try Environment.environVar "subscriptionId" |> Guid.Parse with _ -> failwith "Invalid Subscription ID. This should be your Azure Subscription ID."
+    //     let clientId = try Environment.environVar "clientId" |> Guid.Parse with _ -> failwith "Invalid Client ID. This should be the Client ID of an application registered in Azure with permission to create resources in your subscription."
+    //     let tenantId =
+    //         try Environment.environVarOrNone "tenantId" |> Option.map Guid.Parse
+    //         with _ -> failwith "Invalid TenantId ID. This should be the Tenant ID of an application registered in Azure with permission to create resources in your subscription."
+
+    //     Trace.tracefn "Deploying template '%s' to resource group '%s' in subscription '%O'..." armTemplate resourceGroupName subscriptionId
+    //     subscriptionId
+    //     |> authenticateDevice Trace.trace { ClientId = clientId; TenantId = tenantId }
+    //     |> Async.RunSynchronously
+
+    let authCtxNotDevice =
+        Trace.tracefn "Deploying template using authCtxNotDevice"
         // You can safely replace these with your own subscription and client IDs hard-coded into this script.
         let subscriptionId = try Environment.environVar "subscriptionId" |> Guid.Parse with _ -> failwith "Invalid Subscription ID. This should be your Azure Subscription ID."
         let clientId = try Environment.environVar "clientId" |> Guid.Parse with _ -> failwith "Invalid Client ID. This should be the Client ID of an application registered in Azure with permission to create resources in your subscription."
         let tenantId =
-            try Environment.environVarOrNone "tenantId" |> Option.map Guid.Parse
+            try Environment.environVarOrDefault "tenantId" "" |> Guid.Parse
             with _ -> failwith "Invalid TenantId ID. This should be the Tenant ID of an application registered in Azure with permission to create resources in your subscription."
-
+        let clientSecret = Environment.environVarOrDefault "clientSecret" "clientSecret"
         Trace.tracefn "Deploying template '%s' to resource group '%s' in subscription '%O'..." armTemplate resourceGroupName subscriptionId
         subscriptionId
-        |> authenticateDevice Trace.trace { ClientId = clientId; TenantId = tenantId }
-        |> Async.RunSynchronously
+        |> authenticate { ClientId = clientId; TenantId = tenantId ; ClientSecret = clientSecret}
+
+    let deployment =
+        let location = Environment.environVarOrDefault "location" Region.AustraliaEast.Name
+        let pricingTier = Environment.environVarOrDefault "pricingTier" "F1"
+        { DeploymentName = "SAFE-template-deploy"
+          ResourceGroup = New(resourceGroupName, Region.Create location)
+          ArmTemplate = IO.File.ReadAllText armTemplate
+          Parameters =
+              Simple
+                  [ "environment", ArmString environment
+                    "location", ArmString location
+                    "pricingTier", ArmString pricingTier ]
+          DeploymentMode = Incremental }
+
+    deployment
+    |> deployWithProgress authCtxNotDevice
+    |> Seq.iter(function
+        | DeploymentInProgress (state, operations) -> Trace.tracefn "State is %s, completed %d operations." state operations
+        | DeploymentError (statusCode, message) -> Trace.traceError <| sprintf "DEPLOYMENT ERROR: %s - '%s'" statusCode message
+        | DeploymentCompleted d -> deploymentOutputs <- d)
+)
+
+Target.create "TestDep" (fun _ ->
+    let environment = Environment.environVarOrDefault "environment" (Guid.NewGuid().ToString().ToLower().Split '-' |> Array.head)
+    let armTemplate = @"arm-template.json"
+    let resourceGroupName = "safe-" + environment
 
     let authCtxNotDevice =
         Trace.tracefn "Deploying template using authCtxNotDevice"
@@ -216,7 +255,6 @@ open Fake.Core.TargetOperators
     ==> "Bundle"
     ==> "ArmTemplate"
     ==> "AppService"
-
 
 "Clean"
     ==> "InstallClient"
